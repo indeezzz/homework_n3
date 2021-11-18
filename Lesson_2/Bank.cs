bf8ea64b558de6fc5b121452d242267c2ab23eb7
Indeezzz <indeeezzz@mail.ru>
2021-11-18 20:00:26 +0300

    Домашняя работа №2

---
+++ Lesson_2/Bank.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+
+    class Bank
+    {             
+        public class Account
+        {
+            private string _typeaccount;
+            private Guid _number;
+            private int _balance;
+
+            public Account(string typeaccount, int balance): this(Guid.Empty, typeaccount , balance) { }
+            public Account(string typeaccount) : this(Guid.Empty, typeaccount, 0) { }
+            private Account(Guid number, string typeaccount, int balance)
+            {
+
+                number = Guid.NewGuid();
+                _number = number;
+                _typeaccount = typeaccount;
+                _balance = balance;
+            }     
+            public void GetInfoAccount ()
+            {
+                Console.WriteLine($"Номер вашего счета:{_number} \nТип вашего счета: {_typeaccount} \nБаланс вашего счета: {_balance}");
+            }
+            public void GetMonney (int balance)
+            {                    
+                if (_balance > 0 && _balance > balance )
+                {
+                    _balance = _balance - balance;
+                    Console.WriteLine($"На вашем счету {_number} осталось  : {_balance}");
+                }
+                else
+                {
+                    Console.WriteLine("На вашем счету недостаточно средств для списания");
+                }
+            }
+            public void SetMonney(int balance)
+            {
+                _balance = _balance + balance;
+                Console.WriteLine($"Ваш счет {_number} пополнился : Остаток на счету: {_balance}");
+            }
+        }
+
+        //public class BankPerson : Person
+        //{
+        //    private int _nomerwindow;
+
+        //    public int NomerWindow
+        //    {
+        //        get
+        //        {
+        //            return _nomerwindow;
+        //        }
+        //        set
+        //        {                     
+        //            _nomerwindow = value;
+        //        }
+        //    }
+        //}
+    }
+}
