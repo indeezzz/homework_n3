10b9e54887ca3c22f7da03457d17237ce7378727
Indeezzz <indeeezzz@mail.ru>
2021-11-24 00:12:52 +0300

    Домашняя работа №3

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Lesson_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestBank.TestSystemBank();
+
+            Str str = new Str();
+            str.newStr("abcde");
+        }
+    }
+}
