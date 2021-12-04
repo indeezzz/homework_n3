2b5e166d08875390364f4db0bade9ffb1e72ad48
Indeezzz <indeeezzz@mail.ru>
2021-12-05 02:31:21 +0300

    Домашняя работа №4

---
+++ Lesson_4/Build.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    public class Build
+    {
+        private Guid _number;
+        private int _height;        
+        private int _level;
+        private int _frdoor;
+        private int _apartments;
+
+        public Build(int height, int level, int frdoor, int apartments) : this(Guid.Empty, height, level, frdoor, apartments) { }
+        private Build(Guid number, int height, int level, int frdoor, int apartments)
+        {
+            number = Guid.NewGuid();
+            _number = number;
+            _height = height;
+            _level = level;
+            _frdoor = frdoor;
+            _apartments = apartments;
+        }
+        public void GetFullInfo()
+        {
+            Console.WriteLine($"Уникальный номер дома:{_number} \n Количество этажей в здании: {_level} \n Высота здания: {_height} \n Количество подъездов: {_frdoor} ");
+        }
+        public void GetHeightofHouse()
+        {
+            Console.WriteLine($"Уникальный номер дома:{_number} \n Высота здания: {_height}");
+        }
+        public void GetAparmentsofFrDoor()
+        {
+            int frdoor_appartment = _apartments / _frdoor;
+            Console.WriteLine($"Уникальный номер дома:{_number} \n Количество квартир в подъезде: {frdoor_appartment} \n Количество квартир на этаже: {frdoor_appartment / _level}");
+        }        
+    }
+}
---
+++ Lesson_4/Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+{
+    class Test
+    {
+        public static void TestSystem()
+        {
+            Build[] builders = new Build[]
+            {
+                new Build (100, 3, 6, 72) { },
+                new Build (200, 10, 6, 120) { },
+            };
+
+            builders[0].GetFullInfo();
+            builders[1].GetFullInfo();
+
+            builders[0].GetHeightofHouse();
+            builders[1].GetHeightofHouse();
+
+            builders[0].GetAparmentsofFrDoor();
+            builders[1].GetAparmentsofFrDoor();
+        }
+    }
+}
