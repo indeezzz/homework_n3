a15d9ee39fb29be06009b5c96aa461c115fc0cab
Indeezzz <indeeezzz@mail.ru>
2022-01-22 16:06:48 +0300

    Домашняя работа №5

---
+++ Lesson_5/Logger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+{
+    class Logger
+    {
+        public static void Log(ClassDrob num)
+        {
+            Console.WriteLine($"Целое: {num.Z} Числитель: {num.X}  Знаменатель: {num.Y}");
+        }
+        public static void Log(bool num)
+        {
+            Console.WriteLine($"Данное утверждение: {num} ");
+        }
+        
+    }
+}
